7b2d6bfb9feb9915b4ad429e4e2b2348451f8ac8
ahassh1 <ahasanhabibpolash420@gmail.com>
2026-04-07 01:15:32 +0600

    progress section done

--- src/App.jsx
+++ src/App.jsx
@@ -46,39 +46,54 @@ function App() {
     setCurrentIndex((prev) => prev + 1);
     setSelected("");
   };
+  console.log({ score, userAnswers, setSelected });
+  const totalQuestions = questions.length;
+  const currentQuestionNumber = currentIndex + 1;
 
   return (
     <>
       <div className="min-h-screen bg-base-300 flex items-center justify-center">
         <div>
-          <div>
-            {/* question  */}
-            <h2 className="text-xl font-semibold mb-4">
-              {currentQuestion.title}
-            </h2>
-            {/* options  */}
-            <div className="grid">
-              {currentQuestion.options.map((option, index) => (
-                <button
-                  className={`btn text-start  justify-start px-8 py-5 my-1 ${selected === option ? "btn-primary" : ""}`}
-                  key={index}
-                  onClick={() => setSelected(option)}
-                >
-                  {option}
-                </button>
-              ))}
-            </div>
-
-            {/* next button  */}
-            <div>
+          {/* progress  */}
+          <div className="flex justify-between items-center mb-2">
+            <span className="text-sm">
+              Question {currentQuestionNumber} of {totalQuestions}
+            </span>
+            <span className="badge badge-outline bg-blue-700 text-white">
+              {(currentQuestionNumber / totalQuestions) * 100}%
+            </span>
+          </div>
+          <progress
+            className="progress progress-primary w-full"
+            value={currentQuestionNumber}
+            max={totalQuestions}
+          ></progress>
+          {/* question  */}
+          <h2 className="text-xl font-semibold mb-4">
+            {currentQuestion.title}
+          </h2>
+          {/* options  */}
+          <div className="grid">
+            {currentQuestion.options.map((option, index) => (
               <button
-                className="btn rounded-sm btn-block btn-primary mt-4"
-                disabled={!selected}
-                onClick={handleNext}
+                className={`btn text-start  justify-start px-8 py-5 my-1 ${selected === option ? "btn-primary" : ""}`}
+                key={index}
+                onClick={() => setSelected(option)}
               >
-                Next quiz
+                {option}
               </button>
-            </div>
+            ))}
+          </div>
+
+          {/* next button  */}
+          <div>
+            <button
+              className="btn rounded-sm btn-block btn-primary mt-4"
+              disabled={!selected}
+              onClick={handleNext}
+            >
+              Next quiz
+            </button>
           </div>
         </div>
       </div>
